de838f0d086b99d638d16c8278c0a2e256880608
Kure <christian.parobek@gmail.com>
2015-08-26 09:03:33 -0400

    cleaner version of heatmap

--- heatmap/heatmap.r
+++ heatmap/heatmap.r
@@ -136,11 +136,11 @@ theme <- theme(
   panel.background = element_blank(),
   plot.margin=unit(c(0,0,-0.25,-0.5),"cm"))
 
-bottom_row_theme <- theme(axis.text = element_text(angle = 45, hjust = 1, size = rel(0.8)), axis.text.y = element_blank())
+bottom_row_theme <- theme(axis.text = element_text(angle = 45, hjust = 1, size = rel(0.6)), axis.text.y = element_blank())
 
-left_row_theme <- theme(axis.title = element_text(size = rel(1)), plot.margin=unit(c(0,0,-0.25,0),"cm"), axis.title.x = element_blank())
+left_row_theme <- theme(axis.title = element_text(size = rel(0.8)), plot.margin=unit(c(0,0,-0.25,0),"cm"), axis.title.x = element_blank())
 
-top_row_theme <- theme(plot.margin=unit(c(0.5,0,-0.25,-0.5),"cm"), plot.title = element_text(size = rel(1)))
+top_row_theme <- theme(plot.margin=unit(c(0.75,0,-0.25,-0.5),"cm"), plot.title = element_text(size = rel(0.8)))
 
 ## DEFINE GENOMETRY ##
 geometry <- geom_tile(aes(fill = value), colour = "white", alpha = 0.8)
@@ -176,14 +176,14 @@ p_cp4 <- ggplot(cp4, aesthetics) + geometry + gradient + theme + bottom_row_them
   scale_x_discrete(labels = c("ppq_ic50" = "PPQ", "mq_ic50" = "MQ"))
 
 ## PLOT PPQ COLUMN ##
-p_cp1_ppq <- ggplot(cp1_ppq, aesthetics) + geometry + theme + cp1_color + top_row_theme + labs(title = "PPQ-R")
+p_cp1_ppq <- ggplot(cp1_ppq, aesthetics) + geometry + theme + cp1_color + top_row_theme + labs(title = "PPQR")
 p_cp2_ppq <- ggplot(cp2_ppq, aesthetics) + geometry + theme + cp2_color
 p_cp3_ppq <- ggplot(cp3_ppq, aesthetics) + geometry + theme + cp3_color
 p_cp4_ppq <- ggplot(cp4_ppq, aesthetics) + geometry + theme + cp4_color + bottom_row_theme +
   scale_x_discrete(labels = c("CRT_350" = "crt350", "MDR6_repeats_gt6" = "mdr6", "MDR1_1246" = "mdr1", "MIIg_present" = "MIIg"))
 
 ## PLOT MQ COLUMN ##
-p_cp1_mq <- ggplot(cp1_mq, aesthetics) + geometry + theme + cp1_color + top_row_theme + labs(title = "MQ-R")
+p_cp1_mq <- ggplot(cp1_mq, aesthetics) + geometry + theme + cp1_color + top_row_theme + labs(title = "MQR")
 p_cp2_mq <- ggplot(cp2_mq, aesthetics) + geometry + theme + cp2_color
 p_cp3_mq <- ggplot(cp3_mq, aesthetics) + geometry + theme + cp3_color
 p_cp4_mq <- ggplot(cp4_mq, aesthetics) + geometry + theme + cp4_color + bottom_row_theme +
@@ -198,7 +198,7 @@ p_cp4_k13 <- ggplot(cp4_k13, aesthetics) + geometry + theme + cp4_color + bottom
 
 
 ## PLOT ART COLUMN ##
-p_cp1_art <- ggplot(cp1_art, aesthetics) + geometry + theme + cp1_color + left_row_theme + top_row_theme + theme(plot.margin=unit(c(0.5,0,-0.25,0),"cm")) + labs(title = "Background") + ylab("CP1")
+p_cp1_art <- ggplot(cp1_art, aesthetics) + geometry + theme + cp1_color + left_row_theme + top_row_theme + theme(plot.margin=unit(c(0.75,0,-0.25,0),"cm")) + labs(title = "Background") + ylab("CP1")
 p_cp2_art <- ggplot(cp2_art, aesthetics) + geometry + theme + cp2_color + left_row_theme + ylab("CP2")
 p_cp3_art <- ggplot(cp3_art, aesthetics) + geometry + theme + cp3_color + left_row_theme + ylab("CP3")
 p_cp4_art <- ggplot(cp4_art, aesthetics) + geometry + theme + cp4_color + left_row_theme + bottom_row_theme + ylab("CP4") +
@@ -208,5 +208,7 @@ grid.arrange(p_cp1_art, p_cp1_k13, p_cp1_mq, p_cp1_ppq, p_cp1,
              p_cp2_art, p_cp2_k13, p_cp2_mq, p_cp2_ppq, p_cp2, 
              p_cp3_art, p_cp3_k13, p_cp3_mq, p_cp3_ppq, p_cp3, 
              p_cp4_art, p_cp4_k13, p_cp4_mq, p_cp4_ppq, p_cp4, 
-             ncol = 5, heights=c(0.9,.588,1,1.25), widths=c(3,1,0.5,2,1))
-grid.text("ART-R", x = unit(0.35, "npc"), y = unit(0.95, "npc"))
+             ncol = 5, heights=c(0.9,.588,1,1.25), widths=c(3.2,1,0.5,2,1))
+grid.text("ART-R", x = unit(0.3, "npc"), y = unit(0.975, "npc"), gp=gpar(fontsize=10))
+grid.segments(x0 = unit(0.09, "npc"), y0 = unit(0.96, "npc"),
+              x1 = unit(0.525, "npc"), y1 = unit(0.96, "npc"))
