4d95a13883adec7d9cc8dd61a4bb0a83228643ed
Kure <christian.parobek@gmail.com>
2017-03-19 14:22:07 -0400

    updates to heatmap: correct names and ic scaling; cleaned up code

--- heatmap/heatmap.r
+++ heatmap/heatmap.r
@@ -19,19 +19,18 @@ library(gridExtra)
 ##### READ AND CLEAN DATA ####
 ##############################
 
-data <- read.table("../pca_vs_ic50/data/PPQ_res_2017-01-30.txt", 
+data <- read.table("PPQ_res_2017-01-30_modified_for_heatmap.txt", 
                    header=TRUE, sep = "\t", na.strings = ".")
 # read in data
 # make the periods NAs
 
-data <- data[!is.na(data$WGS_ID),]
+data <- data[!data$WGS_ID == 0,]
 # remove non-WGS samples
 
-data <- data[order(data$pca_group),]
-# sort the data frame by pca group
-
-data$ppq_ic50[data$ppq_ic50 > 200] <- 200 
-# knock down all high ppq values to 150
+data$ppq_ic90[data$ppq_ic90 > 3000] <- 3000 # knock down the really high values
+data$ppq_ic90 <- as.numeric(scale(data$ppq_ic90))
+data$mq_ic50 <- as.numeric(scale(data$mq_ic50))
+# scale the data
 
 
 ##############################
@@ -43,7 +42,7 @@ ic50.subsetter <- function(data, group, order_by) {
   data <- subset(data, select=c("WGS_ID", "ppq_ic90", "mq_ic50"))[data$pca_group == group,] # subset by group
   data[is.na(data)] <- -500
   data$WGS_ID <- with(data, eval(parse(text = paste("reorder(WGS_ID,", order_by, ")")))) # order
-    # need the eval() expression to pass the "order_by" string as part of the reorder() expression
+  # need the eval() expression to pass the "order_by" string as part of the reorder() expression
   #data <- na.omit(data) # remove rows with missing ic50s
   data <- melt(data) # for ggplot
   return(data)
@@ -51,80 +50,52 @@ ic50.subsetter <- function(data, group, order_by) {
 
 cp1 <- ic50.subsetter(data, 1, "mq_ic50")
 cp2 <- ic50.subsetter(data, 2, "mq_ic50")
-cp3 <- ic50.subsetter(data, 3, "ppq_ic50")
-cp4 <- ic50.subsetter(data, 4, "ppq_ic50")
+cp3 <- ic50.subsetter(data, 3, "ppq_ic90")
+cp4 <- ic50.subsetter(data, 4, "ppq_ic90")
 
-## by ppq genotypes
-ppq.subsetter <- function(data, group, order_by) {
-  data <- subset(data, select=c("WGS_ID", "exo_E415G", "Plas_2_3_dup", "mIIg_present", "X5r_cn_high", "CRT_350", "cviet_present", "MDR6_repeats_gt6", "ppq_ic50", "mq_ic50"))[data$pca_group == group,] # subset by group
-  data[,c('ppq_ic50','mq_ic50')][is.na(data[,c('ppq_ic50','mq_ic50')])] <- -50 # replace mq and ppq ic50 NA values with an integer, -50
+## by generic genotype subsetter
+subsetter <- function(data, group, order_by, selection) {
+  data <- subset(data, select=selection)[data$pca_group == group,] # subset by group
+  data[,c('ppq_ic90','mq_ic50')][is.na(data[,c('ppq_ic90','mq_ic50')])] <- -50 # replace mq and ppq ic50 NA values with an integer, -50
   data$WGS_ID <- with(data, eval(parse(text = paste("reorder(WGS_ID,", order_by, ")")))) # order
-  #data <- data[!with(data,is.na(ppq_ic50)),] # exclude rows w/NA in ppq col
-  #data <- data[!with(data,is.na(mq_ic50)),] # exclude rows w/NA in mq col
-  data <- subset(data, select=-c(ppq_ic50, mq_ic50)) # remove the ppq and mq ic50 cols
+  data <- subset(data, select=-c(ppq_ic90, mq_ic50)) # remove the ppq and mq ic50 cols
   data <- data.frame(lapply(data, as.factor)) # convert all cols to factors
   data <- melt(data, id.vars = "WGS_ID")
   return(data)
 }
 
-cp1_ppq <- ppq.subsetter(data, 1, "mq_ic50")
-cp2_ppq <- ppq.subsetter(data, 2, "mq_ic50")
-cp3_ppq <- ppq.subsetter(data, 3, "ppq_ic50")
-cp4_ppq <- ppq.subsetter(data, 4, "ppq_ic50")
+## subset ppq blocks
+ppq_cols <- c("WGS_ID", "exo_E415G", "Plas_2_3_dup", 
+              "MIIg_present_new", "X5r_cn_high", "CRT_350", 
+              "cviet_present", "MDR6_repeats_gt6", "ppq_ic90", "mq_ic50")
+cp1_ppq <- subsetter(data, 1, "mq_ic50", ppq_cols)
+cp2_ppq <- subsetter(data, 2, "mq_ic50", ppq_cols)
+cp3_ppq <- subsetter(data, 3, "ppq_ic90", ppq_cols)
+cp4_ppq <- subsetter(data, 4, "ppq_ic90", ppq_cols)
 
-## by mq genotypes ###### Turn this into names
-mq.subsetter <- function(data, group, order_by) {
-  data <- subset(data, select=c("WGS_ID", "mdrcn_high", "ppq_ic50", "mq_ic50"))[data$pca_group == group,] # subset by group
-  data[,c('ppq_ic50','mq_ic50')][is.na(data[,c('ppq_ic50','mq_ic50')])] <- -50 # replace mq and ppq ic50 NA values with an integer, -50
-  data$WGS_ID <- with(data, eval(parse(text = paste("reorder(WGS_ID,", order_by, ")")))) # order
-  #data <- data[!with(data,is.na(ppq_ic50)),] # exclude rows w/NA in ppq col
-  #data <- data[!with(data,is.na(mq_ic50)),] # exclude rows w/NA in mq col
-  data <- subset(data, select=-c(ppq_ic50, mq_ic50)) # remove the ppq and mq ic50 cols
-  data <- data.frame(lapply(data, as.factor)) # convert all cols to factors
-  data <- melt(data, id.vars = "WGS_ID")
-  return(data)
-}
-
-cp1_mq <- mq.subsetter(data, 1, "mq_ic50")
-cp2_mq <- mq.subsetter(data, 2, "mq_ic50")
-cp3_mq <- mq.subsetter(data, 3, "ppq_ic50")
-cp4_mq <- mq.subsetter(data, 4, "ppq_ic50")
+## by mq genotypes
+mq_cols <- c("WGS_ID", "mdrcn_high", "ppq_ic90", "mq_ic50")
+cp1_mq <- subsetter(data, 1, "mq_ic50", mq_cols)
+cp2_mq <- subsetter(data, 2, "mq_ic50", mq_cols)
+cp3_mq <- subsetter(data, 3, "ppq_ic90", mq_cols)
+cp4_mq <- subsetter(data, 4, "ppq_ic90", mq_cols)
 
 ## by art genotypes (Miotto's backbone)
-art.subsetter <- function(data, group, order_by) {
-  data <- subset(data, select=c("WGS_ID", "miotto_mdr2", "miotto_arps10", "miotto_crt326", "miotto_crt356", "miotto_fd", "miotto_pph", "ppq_ic50", "mq_ic50"))[data$pca_group == group,]
-  data[,c('ppq_ic50','mq_ic50')][is.na(data[,c('ppq_ic50','mq_ic50')])] <- -50 # replace mq and ppq ic50 NA values with an integer, -50
-  data$WGS_ID <- with(data, eval(parse(text = paste("reorder(WGS_ID,", order_by, ")")))) # order
-  #data <- data[!with(data,is.na(ppq_ic50)),] # exclude rows w/NA in ppq col
-  #data <- data[!with(data,is.na(mq_ic50)),] # exclude rows w/NA in mq col
-  data <- subset(data, select=-c(ppq_ic50, mq_ic50)) # remove the ppq and mq ic50 cols
-  data <- data.frame(lapply(data, as.factor)) # convert all cols to factors
-  data <- melt(data, id.vars = "WGS_ID")
-  return(data)
-}
-
-cp1_art <- art.subsetter(data, 1, "mq_ic50")
-cp2_art <- art.subsetter(data, 2, "mq_ic50")
-cp3_art <- art.subsetter(data, 3, "ppq_ic50")
-cp4_art <- art.subsetter(data, 4, "ppq_ic50")
+miotto_cols <- c("WGS_ID", "miotto_mdr2", "miotto_arps10", 
+                 "miotto_crt326", "miotto_crt356", "miotto_fd", 
+                 "miotto_pph", "ppq_ic90", "mq_ic50")
+cp1_art <- subsetter(data, 1, "mq_ic50", miotto_cols)
+cp2_art <- subsetter(data, 2, "mq_ic50", miotto_cols)
+cp3_art <- subsetter(data, 3, "ppq_ic90", miotto_cols)
+cp4_art <- subsetter(data, 4, "ppq_ic90", miotto_cols)
 
 ## by K13 genotypes
-k13.subsetter <- function(data, group, order_by) {
-  data <- subset(data, select=c("WGS_ID", "k13_r539t_present", "k13_c580y_present", "k13_other_present", "ppq_ic50", "mq_ic50"))[data$pca_group == group,]
-  data[,c('ppq_ic50','mq_ic50')][is.na(data[,c('ppq_ic50','mq_ic50')])] <- -50 # replace mq and ppq ic50 NA values with an integer, -50
-  data$WGS_ID <- with(data, eval(parse(text = paste("reorder(WGS_ID,", order_by, ")")))) # order
-  #data <- data[!with(data,is.na(ppq_ic50)),] # exclude rows w/NA in ppq col
-  #data <- data[!with(data,is.na(mq_ic50)),] # exclude rows w/NA in mq col
-  data <- subset(data, select=-c(ppq_ic50, mq_ic50)) # remove the ppq and mq ic50 cols
-  data <- data.frame(lapply(data, as.factor)) # convert all cols to factors
-  data <- melt(data, id.vars = "WGS_ID")
-  return(data)
-}
-
-cp1_k13 <- k13.subsetter(data, 1, "mq_ic50")
-cp2_k13 <- k13.subsetter(data, 2, "mq_ic50")
-cp3_k13 <- k13.subsetter(data, 3, "ppq_ic50")
-cp4_k13 <- k13.subsetter(data, 4, "ppq_ic50")
+k13_cols <- c("WGS_ID", "k13_r539t_present", "k13_c580y_present", 
+              "k13_other_present", "ppq_ic90", "mq_ic50")
+cp1_k13 <- subsetter(data, 1, "mq_ic50", k13_cols)
+cp2_k13 <- subsetter(data, 2, "mq_ic50", k13_cols)
+cp3_k13 <- subsetter(data, 3, "ppq_ic90", k13_cols)
+cp4_k13 <- subsetter(data, 4, "ppq_ic90", k13_cols)
 
 ##############################
 ##### DEFINE PLOT PARAMS #####
@@ -151,11 +122,13 @@ top_row_theme <- theme(plot.margin=unit(c(0.75,0,-0.25,-0.5),"cm"), plot.title =
 geometry <- geom_tile(aes(fill = value), colour = "white", alpha = 0.8)
 
 ## DEFINE PLOT ##
-aesthetics <- aes(variable, WGS_ID)
+aesthetics <- aes(as.factor(variable), WGS_ID, group=WGS_ID)
 
-## Colorbrewer recommended colors
-gradient <- scale_fill_gradient(low = "grey90", high = "black", limits = c(-50,200), na.value = "#ffffbf")
-bw <- scale_fill_manual(values = c("0" = "#91bfdb", "1" = "#fc8d59"), na.value = "#ffffbf")
+## Setting colors for gradient and binary 
+min <- min(c(data$ppq_ic90, data$mq_ic50), na.rm = TRUE)
+max <- max(c(data$ppq_ic90, data$mq_ic50), na.rm = TRUE)
+gradient <- scale_fill_gradient(low = "grey90", high = "black", limits = c(min, max), na.value = "#ffffbf")
+bw <- scale_fill_manual(values = c("0" = "#91bfdb", "1" = "#fc8d59"), na.value = "#ffffbf") # original way
 
 
 #################################
@@ -192,6 +165,12 @@ p_cp2_art <- ggplot(cp2_art, aesthetics) + geometry + theme + bw
 p_cp3_art <- ggplot(cp3_art, aesthetics) + geometry + theme + bw
 p_cp4_art <- ggplot(cp4_art, aesthetics) + geometry + theme + bw
 
+## Plot Label plots
+cp1_lab <- ggplot(cp1[cp1$variable == "ppq_ic90",], aesthetics) + geom_text(aes(label=WGS_ID), size = 2) + theme
+cp2_lab <- ggplot(cp2[cp2$variable == "ppq_ic90",], aesthetics) + geom_text(aes(label=WGS_ID), size = 2) + theme
+cp3_lab <- ggplot(cp3[cp3$variable == "ppq_ic90",], aesthetics) + geom_text(aes(label=WGS_ID), size = 2) + theme
+cp4_lab <- ggplot(cp4[cp4$variable == "ppq_ic90",], aesthetics) + geom_text(aes(label=WGS_ID), size = 2) + theme
+
 
 ###############################
 ########## PLOT IT ############
@@ -199,10 +178,10 @@ p_cp4_art <- ggplot(cp4_art, aesthetics) + geometry + theme + bw
 
 svg("heatmap_ic90.svg", width=5, height=8)
 
-grid.arrange(p_cp1_art, p_cp1_k13, p_cp1_mq, p_cp1_ppq, p_cp1, p_cp2_art, p_cp2_k13, p_cp2_mq, p_cp2_ppq, p_cp2, 
-             p_cp3_art, p_cp3_k13, p_cp3_mq, p_cp3_ppq, p_cp3, p_cp4_art, p_cp4_k13, p_cp4_mq, p_cp4_ppq, p_cp4, 
-             ncol = 5, widths = c(6/19,3/19,1/19,7/19,2/19), heights = c(17/78, 18/78, 20/78, 23/78),
-             top = "", left = "", bottom = "", right = "", padding = unit(1, "line"))
+grid.arrange(p_cp1_art, p_cp1_k13, p_cp1_mq, p_cp1_ppq, p_cp1, cp1_lab, p_cp2_art, p_cp2_k13, p_cp2_mq, p_cp2_ppq, p_cp2, cp2_lab,
+             p_cp3_art, p_cp3_k13, p_cp3_mq, p_cp3_ppq, p_cp3, cp3_lab, p_cp4_art, p_cp4_k13, p_cp4_mq, p_cp4_ppq, p_cp4, cp4_lab,
+             ncol = 6, widths = c(6/19,3/19,1/19,7/19,2/19,1.5/19), heights = c(17/78, 18/78, 20/78, 23/78),
+             top = "", left = "", bottom = "", padding = unit(1, "line"))
 
 ## Place column-group labels
 grid.text("Background", x = 0.21, y = 0.985, gp=gpar(fontsize=10))
@@ -210,12 +189,6 @@ grid.text("K13", x = 0.415, y = 0.985, gp=gpar(fontsize=10))
 grid.text("MQ", x = 0.505, y = 0.985, gp=gpar(fontsize=10))
 grid.text("PPQ", x = 0.69, y = 0.985, gp=gpar(fontsize=10))
 grid.text("IC90/50", x = 0.895, y = 0.985, gp=gpar(fontsize=10))
-
-## Add the sample IDs
-grid.text(rev(data[data$pca_group=="1",3]), x=0.97, y = ((0:16)/87)+0.770, gp=gpar(fontsize=6))
-grid.text(rev(data[data$pca_group=="2",3]), x=0.97, y = ((0:17)/87)+0.558, gp=gpar(fontsize=6))
-grid.text(rev(data[data$pca_group=="3",3]), x=0.97, y = ((0:19)/87)+0.323, gp=gpar(fontsize=6))
-grid.text(rev(data[data$pca_group=="4",3]), x=0.97, y = ((0:22)/87)+0.053, gp=gpar(fontsize=6))
 
 ## Place line segments over columns
 grid.segments(x0 = 0.08, y0 = 0.97, x1 = 0.335, y1 = 0.97)
@@ -238,4 +211,5 @@ grid.text(c("mdr1"), rot = 45, x = 0.51, y = 0.04, gp=gpar(fontsize=6), just = "
 grid.text(c("PPQ", "MQ"), rot = 45, x = ((0:1)/23)+0.875, y = 0.04, gp=gpar(fontsize=6), just = "right")
 
 dev.off()
+
 
